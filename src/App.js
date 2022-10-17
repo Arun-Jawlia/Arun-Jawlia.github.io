eb2a010d3457436898dfee26f152d5cc941ecada
Arun-Jawlia <arunkumar08.mk@gmail.com>
2022-10-17 18:41:52 +0530

    portfolioNav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './Component/Navbar';
+import { AllRoutes } from './Pages/AllRoutes';
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello world</h1>
-    </div>
+    <>
+    <Container>
+      <NavbarDiv>
+      <Navbar/>
+
+      </NavbarDiv>
+
+      <AllRoutes/>
+    </Container>
+    </>
   );
+
 }
 
 export default App;
+
+
+const Container = styled.div`
+  display: flex;
+  border:1px solid white;
+  // background-color:teal
+  `
+const NavbarDiv = styled.div`
+color:blue;
+`
+
